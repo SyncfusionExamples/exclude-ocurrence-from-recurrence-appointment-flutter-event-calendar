fc4d8a8a4eb7095ba14fbdd2d5e6c79098ea80eb
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-10 12:04:59 +0530

    FLUT-3143 - Sample and screenshot added.

---
+++ android/app/src/main/kotlin/com/example/flutter_recurrenceexception/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_recurrenceexception
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+void main() => runApp(RecurrenceException());
+
+class RecurrenceException extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: SfCalendar(
+            view: CalendarView.week,
+            dataSource: _getCalendarDataSource(),
+          ),
+        ),
+
+        // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+
+  _AppointmentDataSource _getCalendarDataSource() {
+    List<Appointment> appointments = <Appointment>[];
+    DateTime exceptionDate = DateTime(2020, 12, 23);
+    appointments.add(Appointment(
+        startTime: DateTime.now(),
+        endTime: DateTime.now().add(Duration(hours: 1)),
+        subject: 'Meeting',
+        color: Colors.pink,
+        recurrenceRule: 'FREQ=DAILY;COUNT=20',
+        recurrenceExceptionDates: <DateTime>[exceptionDate]));
+
+    return _AppointmentDataSource(appointments);
+  }
+}
+
+class _AppointmentDataSource extends CalendarDataSource {
+  _AppointmentDataSource(List<Appointment> source) {
+    appointments = source;
+  }
+}
